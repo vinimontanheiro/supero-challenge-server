d195fab9c0b86eca6aa79c9fc3d09b5f8284aff7
Vinícius <mrvini@live.com>
2020-02-05 18:20:59 -0300

    Fixing sql

--- docker/postgres/migrations/sql/V0001__create_account_table.sql
+++ docker/postgres/migrations/sql/V0001__create_account_table.sql
@@ -1,11 +1,4 @@
-CREATE DATABASE supero_challenge
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Portuguese_Brazil.1252'
-    LC_CTYPE = 'Portuguese_Brazil.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
+CREATE DATABASE supero_challenge;
 
 CREATE TABLE account(
    id serial PRIMARY KEY,
